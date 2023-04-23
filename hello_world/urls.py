1067e9dc75fc8cdcf6b35d1895a83067413ea228
HoKit <Hokit@git.com>
2023-04-23 21:07:22 +0800

    ok

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -5,4 +5,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^helloworld/$', views.helloworld),
 ]
+
+# urlpatterns = (
+#     # url(r'^$', views.home),
+#     # url(r'^dev-guide/$', views.dev_guide),
+#     # url(r'^contact/$', views.contact),
+#     # # 添加 helloworld 路由
+#     url(r'^helloworld/$', views.helloworld),
+# )
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. Your blueking app is running successfully now.")
+    
+def helloworld(request):
+    return render(request, 'hello_world/helloworld.html')
